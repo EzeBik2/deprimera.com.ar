52a76128f9b755fb96f17f0edc9a22e9fcaa5d68
42657431 <42657431@A-CVI-06>
2017-11-07 10:35:02 -0300

    1

--- deprimera.com.ar/deprimera.com.ar/Models/Jugadores.cs
+++ deprimera.com.ar/deprimera.com.ar/Models/Jugadores.cs
@@ -14,7 +14,7 @@ namespace deprimera.com.ar.Models
         static MySqlConnection connMySQL = new MySqlConnection();
         private static void ConectarDB()
         {
-            connMySQL.ConnectionString = @"Database=localdb;Data Source=127.0.0.1;User Id=azure;Password=6#vWHD_$;Port:49164";
+            connMySQL.ConnectionString = @"Database=localdb;Data Source=127.0.0.1;User Id=azure;Password=6#vWHD_$";
             connMySQL.Open();
         }
         public static Jugador TraerUnJugadorPorEmailClave(Jugador unJugador)
