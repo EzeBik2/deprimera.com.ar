949e7badac383f014a98c0af5ec8cf5ca1d22aa4
SurixOne <martuslalv@hotmail.com>
2017-11-08 22:12:25 -0300

    CambiosColo1

--- deprimera.com.ar/deprimera.com.ar/Models/PartidosJugadores.cs
+++ deprimera.com.ar/deprimera.com.ar/Models/PartidosJugadores.cs
@@ -16,7 +16,7 @@ namespace deprimera.com.ar.Models
 
         private static void ConectarDB()
         {
-            connMySQL.ConnectionString = @"Database=dbdeprimera;Data Source=localhost;User Id=root;Password=root";
+            connMySQL.ConnectionString = @"Database=localdb;Data Source=127.0.0.1;User Id=azure;Password=6#vWHD_$";
             connMySQL.Open();
         }
         public static PartidoJugador AgregarJugadorAPartido(PartidoJugador unPartidoJugador)
